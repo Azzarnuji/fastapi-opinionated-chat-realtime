cb9b420aed6c6758f1e0391e8f2482abf3940d8a
Azzarnuji <azzarnuzy93@gmail.com>
2025-11-16 05:28:34 +0700

    [Azzar] feat: Mengimplementasikan aktivasi otomatis plugin Socket dan EventBus setelah publikasi.

--- fastapi_opinionated_chat_realtime/publish/publish.py
+++ fastapi_opinionated_chat_realtime/publish/publish.py
@@ -1,7 +1,12 @@
 # fastapi-opinionated-chat-realtime/fastapi_opinionated_chat_realtime/publish/publish.py
 from fastapi_opinionated.shared.publish_metadata import PublishMetadata
+import subprocess
 
 class ChatRealtimePublish(PublishMetadata):
     domain: str = "chat_realtime"
     overwrite: bool = False
     overwrite_rules: dict[str, bool] = {}
+    
+    async def post_publish(self):
+        subprocess.check_call(["fastapi-opinionated", "plugins", "enable", "fastapi_opinionated_socket.plugin.SocketPlugin"])
+        subprocess.check_call(["fastapi-opinionated", "plugins", "enable", "fastapi_opinionated_eventbus.plugin.EventBusPlugin"])
